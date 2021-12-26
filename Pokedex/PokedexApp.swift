eeeb74d611ea42ee7c6098978a61106d8399c959
hicka04 <hicka04@gmail.com>
2021-12-26 22:42:51 +0900

    Add project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by hicka04 on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
