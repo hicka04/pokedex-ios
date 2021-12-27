aee091f3bccc4ab2a3dca8a70ec1d681a296c32a
hicka04 <hicka04@gmail.com>
2021-12-27 23:03:51 +0900

    Add Infra package

---
+++ Infra/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Infra",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Infra",
+            targets: ["Infra"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "APIKit", url: "https://github.com/ishkawa/APIKit.git", .upToNextMajor(from: "5.2.0"))
+    ],
+    targets: [
+        .target(
+            name: "Infra",
+            dependencies: [
+                "APIKit"
+            ]
+        ),
+        .testTarget(
+            name: "InfraTests",
+            dependencies: ["Infra"]
+        )
+    ]
+)
---
+++ Infra/README.md
@@ -0,0 +1,3 @@
+# Infra
+
+A description of this package.
