e203505c4edea500b19759c77a792f3f9c103a2a
hicka04 <hicka04@gmail.com>
2022-06-02 00:24:13 +0900

    Run SwiftGen using Swift Package Manager Plugin

--- DesignSystem/Package.swift
+++ DesignSystem/Package.swift
@@ -19,11 +19,26 @@ let package = Package(
         .package(name: "Core", path: "../Core")
     ],
     targets: [
+        .binaryTarget(
+            name: "swiftgen",
+            path: "Artifacts/swiftgen.artifactbundle"
+        ),
+        .plugin(
+            name: "SwiftGenPlugin",
+            capability: .buildTool(),
+            dependencies: ["swiftgen"]
+        ),
         .target(
             name: "DesignSystem",
             dependencies: [
                 .product(name: "Entity", package: "Domain"),
                 "Core"
+            ],
+            resources: [
+                .process("Resources")
+            ],
+            plugins: [
+                .plugin(name: "SwiftGenPlugin")
             ]
         )
     ]
