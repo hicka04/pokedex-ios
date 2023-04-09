3c34a899e27ec2203ea265c78670cd207fe43de8
hicka04 <hicka04@gmail.com>
2023-04-09 14:07:51 +0900

    Add Spacing

---
+++ DesignSystem/Sources/Foundations/Spacing.swift
@@ -0,0 +1,14 @@
+//
+//  Spacing.swift
+//  
+//
+//  Created by hicka04 on 2023/04/09.
+//
+
+import Foundation
+
+public enum Spacing: CGFloat {
+    case small = 8
+    case medium = 16
+    case large = 32
+}
