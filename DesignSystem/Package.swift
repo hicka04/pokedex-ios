f5ab7e56913b337aff46da77d4d6f59dd0e5fe0f
hicka04 <hicka04@gmail.com>
2023-04-24 22:33:58 +0900

    Add SwiftLint to DesignSystem

--- DesignSystem/Package.swift
+++ DesignSystem/Package.swift
@@ -15,19 +15,24 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.0")
+        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.0"),
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.51.0")
     ],
     targets: [
         .target(
             name: "DesignSystem",
             dependencies: [
                 "Foundations"
+            ],
+            plugins: [
+                .plugin(name: "SwiftLintPlugin", package: "SwiftLint")
             ]
         ),
         .target(
             name: "Foundations",
             plugins: [
-                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin")
+                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin"),
+                .plugin(name: "SwiftLintPlugin", package: "SwiftLint")
             ]
         )
     ]
